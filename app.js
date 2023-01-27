58f07e0b59376557bdd714ee1d1333571c2fb457
Azadkumar <kumarazad2917@gmail.com>
2023-01-27 08:27:09 +0530

    add links

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const twitter = document.querySelector('.twitter');
+
+const linkedin = document.querySelector('.linkedin');
+
+const github = document.querySelector('.github');
+
+twitter.addEventListener('click',()=>{
+
+    window.open('https://twitter.com/kumarazad2917');
+
+});
+
+linkedin.addEventListener('click',()=>{
+
+    window.open('https://www.linkedin.com/in/azad-kumar-395798218/');
+
+});
+
+github.addEventListener('click',()=>{
+
+    window.open('https://github.com/AzadTom');
+
+});
